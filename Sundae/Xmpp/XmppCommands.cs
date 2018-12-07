6da6be1a18882bea72440060a571964a332a91a6
Talles L <talleslasmar@gmail.com>
2018-12-07 10:03:12 -0200

    better id and random

--- Sundae/Xmpp/XmppCommands.cs
+++ Sundae/Xmpp/XmppCommands.cs
@@ -1,9 +1,16 @@
 namespace Sundae
 {
     using System;
+    using System.Threading;
 
     public static class XmppCommands
     {
+        private static int _id = 0;
+
+        private static int Id => Interlocked.Increment(ref _id);
+
+        private static string Random => new Random().Next().ToString("x");
+
         internal static void OpenStream(this XmppStream stream, string domain) =>
             stream.Write($"<stream:stream to='{domain}' xmlns='jabber:client' " +
                 "xmlns:stream='http://etherx.jabber.org/streams' version='1.0'>");
@@ -13,18 +20,18 @@ namespace Sundae
         public static void Authenticate(
             this XmppConnection xmpp, string user, string password, string resource = null) =>
             xmpp.SendCustom($@"
-                <iq id='{Random()}' type='set'>
+                <iq id='{Random}' type='set'>
                     <query xmlns='jabber:iq:auth'>
                         <username>{user}</username>
                         <password>{password}</password>
-                        <resource>{resource ?? Random("resource")}</resource>
+                        <resource>{Random}</resource>
                     </query>
                 </iq>
             ");
 
         public static void Message(this XmppConnection xmpp, string message, string jid) =>
             xmpp.SendCustom($@"
-                <message id='{Random()}' type='chat' to='{jid}'>
+                <message id='{Random}' type='chat' to='{jid}'>
                     <body>{message}</body>
                 </message>
             ");
@@ -36,18 +43,12 @@ namespace Sundae
 
         public static void Register(this XmppConnection xmpp, string user, string password) =>
             xmpp.SendCustom($@"
-                <iq id='{Random()}' type='set' to='{xmpp.Domain}'>
+                <iq id='{Random}' type='set' to='{xmpp.Domain}'>
                     <query xmlns='jabber:iq:register'>
                         <username>{user}</username>
                         <password>{password}</password>
                     </query>
                 </iq>
             ");
-
-        private static string Random(string text = null)
-        {
-            var random = new Random().Next().ToString();
-            return text == null ? random : $"{text}-{random}";
-        }
     }
 }
